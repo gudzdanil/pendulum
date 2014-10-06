9aba1be2eb92d1e3eede0aa5c69787e9c056770b
Gudz Danil <gudz.danil@gmail.com>
2014-10-07 01:18:20 +0300

    refactored drawing

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
 function Pendulum(node){
 	var style = window.getComputedStyle(node);
+	var info = {
+		start_point:{
+			x: (parseInt(style.width)/2),
+			y: 0
+		},
+		thread: {
+			length: 100
+		},
+		ball: {
+			width: 40,
+			height: 40,
+		}
+	};
+
+	var container = document.createElement('div');
+	container.style.position = 'absolute';
+	container.style.width = 0;
+	container.style.height = info.thread.length + info.ball.height + 'px';
+	container.style.left = info.start_point.x + 'px';
+	container.style.top = info.start_point.y + 'px';
+
 	var thread = document.createElement('div');
-	thread.style.height = '100px';
+	thread.style.height = info.thread.length + 'px';
 	thread.style.width = '1px';
 	thread.style.backgroundColor = 'black';
 	thread.style.position = 'absolute';
-	thread.style.top = 0;
-	thread.style.left = (parseInt(style.width) / 2)+"px";
-	node.appendChild(thread);
+	thread.style.top = 0+ 'px';
+	thread.style.left = 0 + "px";
+	container.appendChild(thread);
 
 	var ball = document.createElement('div');
-	ball.style.width = ball.style.height = '40px';
-	ball.style.borderRadius = '20px';
+	ball.style.width = ball.style.height = info.ball.width + 'px';
+	ball.style.borderRadius = info.ball.width/2 + 'px';
 	ball.style.border = '1px solid black';
 	ball.style.position = 'absolute';
-	ball.style.top = parseInt(thread.style.height) + 'px';
-	ball.style.left = (parseInt(style.width)/2 - parseInt(ball.style.width)/2) + 'px';
-	node.appendChild(ball);
+	ball.style.top = info.thread.length + 'px';
+	ball.style.left = -info.ball.width/2 + 'px';
+	container.appendChild(ball);
+
+	node.appendChild(container);
 }
