b900cbdda9c775743d1b3f78c740a9ee6ab431bf
Gudz Danil <gudz.danil@gmail.com>
2014-10-07 01:28:28 +0300

    implemented redraw setters

--- script.js
+++ script.js
@@ -40,4 +40,37 @@ function Pendulum(node){
 	container.appendChild(ball);
 
 	node.appendChild(container);
+
+	var angle = 0;
+
+	this.setPos = function(x, y, redraw){
+		info.start_point.x = x;
+		info.start_point.y = y;
+		if(redraw)
+			this.reDraw();
+	};
+	this.ballResize = function (w, h, redraw){
+		info.ball.width = w;
+		info.ball.height = h;
+		if(redraw)
+			this.reDraw();
+	};
+	this.setLength = function(l, redraw){
+		info.thread.length = l;
+		if(redraw)
+			this.reDraw();
+	};
+
+	this.reDraw = function(){
+		container.style.height = info.thread.length + info.ball.height + 'px';
+		container.style.left = info.start_point.x + 'px';
+		container.style.top = info.start_point.y + 'px'; 
+
+		ball.style.width = ball.style.height = info.ball.width + 'px';
+		ball.style.borderRadius = info.ball.width/2 + 'px';
+		ball.style.top = info.thread.length + 'px';
+		ball.style.left = -info.ball.width/2 + 'px';
+
+		thread.style.height = info.thread.length + 'px';
+	}
 }
